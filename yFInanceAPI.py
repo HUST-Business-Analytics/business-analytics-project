ea259353c83ba740f5e43a37c3039597019114c2
sunlinecode <torben.smid@gmail.com>
2023-12-06 04:40:12 +0100

    Questions Analysis

--- yFInanceAPI.py
+++ yFInanceAPI.py
@@ -2,7 +2,7 @@ import yfinance as yf
 
 symbol = "MSFT"
 sp500 = yf.Ticker(symbol)
-historical_data = sp500.history(period="3y")  # Fetch data for the last year
+historical_data = sp500.history(period="10y")  # Fetch data for the last year
 selected_data = historical_data[['Open', 'High', 'Low', 'Close', 'Volume']]
 selected_data['Date'] = historical_data.index.date  # Adding the 'Date' column
 selected_data = selected_data.reindex(columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
